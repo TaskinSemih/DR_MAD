3c6b2326acf8fa68bcccf975cc4bf31fdb7d4f73
kylian-strub <kylian.strub@icloud.com>
2025-01-17 16:32:18 +0100

    commit - page dacceuil

---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <h1>Dr Mad app</h1>
+    <p>Bienvenue chez dr Mad</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeView",
+};
+</script>
+
+<style scoped>
+.home {
+  text-align: center;
+  margin: 50px;
+}
+
+h1 {
+  color: #007bff;
+}
+</style>
